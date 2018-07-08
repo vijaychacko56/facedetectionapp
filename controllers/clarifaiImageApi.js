d4d411997463c26a6d0cbe91c2c93a0b4e758640
Vijay <vijay.chacko56@gmail.com>
2018-07-08 14:15:42 -0500

    correcting a syntax in api key clarifai

--- controllers/clarifaiImageApi.js
+++ controllers/clarifaiImageApi.js
@@ -1,7 +1,7 @@
  const  Clarifai  = require('clarifai');
 
  const clarifaiObject = new Clarifai.App({
- apiKey: process.env.CLARIFAI_API_KEY;
+ apiKey: process.env.CLARIFAI_API_KEY
 });
 
  const clarifaiimageApiHandler = () => (req,res) =>{
